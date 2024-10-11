6c6042a0b3eeea1145369ed5d8e4eb03f20f9857
deck <anton.erykov@gmail.com>
2024-10-11 11:17:02 +0200

    перенес файлы шаблонов в отдельную папку, написал мини инструкцию

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # start_linux
+git clone https://github.com/DecK00/project /root/project/
+cd /root/project/
+cp .env.exemple .env
+nano .env
+bash main.sh
--- main.sh
+++ main.sh
@@ -27,22 +27,44 @@ else
     echo "Создаю сеть Docker с именем 'proxy'..."
     docker network create proxy
 
+    echo "Проверяю наличие директории /root/project/3x-ui..."
     if [ ! -d "/root/project/3x-ui" ]; then
-        echo "Директория /root/3x-ui не существует. Клонирую репозиторий..."
+        echo "Директория /root/3x-ui не найдена. Клонирую репозиторий..."
         git clone https://github.com/MHSanaei/3x-ui
     else
         echo "Директория /root/project/3x-ui уже существует."
+    fi
+
+    echo "Проверяю наличие директории /root/project/traefik..."
+    if [ ! -d "/root/project/traefik" ]; then
+        echo "Директория /root/project/traefik не найдена. Создаю директорию..."
+        mkdir -p /root/project/traefik
+    else
+        echo "Директория /root/project/traefik уже существует."
+    fi
+
+    echo "Проверяю наличие директории /root/project/portainer..."
+    if [ ! -d "/root/project/portainer" ]; then
+        echo "Директория /root/project/portainer не найдена. Создаю директорию..."
+        mkdir -p /root/project/portainer
+    else
+        echo "Директория /root/project/portainer уже существует."
     fi
 
     # Создание хеша пароля
     echo "Создаю хеш пароля для пользователя 'admin'..."
     HASH_PASSWORD=$(htpasswd -bnB "admin" "$PASSWORD" | sed -e 's/\$/\$\$/g')
 
-    echo "Настраиваю файл acme.json..."
+    echo "Копирую файлы конфигурации Traefik..."
+    cp /root/project/template/traefik/acme.json /root/project/traefik/acme.json
+    cp /root/project/template/traefik/config.yml /root/project/traefik/config.yml
+    cp /root/project/template/traefik/docker-compose.yml /root/project/traefik/docker-compose.yml
+    cp /root/project/template/traefik/traefik.yml /root/project/traefik/traefik.yml
+
+    echo "Настраиваю файл acme.json для Traefik..."
     chmod u=rw,go= /root/project/traefik/acme.json
-    echo "Копирую файл docker-compose.yml для Traefik..."
-    cp /root/project/traefik_docker-compose.yml /root/project/traefik/docker-compose.yml
-    echo "Заменяю параметры в файле docker-compose.yml для Traefik..."
+
+    echo "Заменяю параметры в docker-compose.yml для Traefik..."
     sed -i -e "s|PASS|$HASH_PASSWORD|g" /root/project/traefik/docker-compose.yml
     sed -i -e "s|URL|$URL|g" /root/project/traefik/docker-compose.yml
     sed -i -e "s|CF_EMAIL|$CF_EMAIL|g" /root/project/traefik/docker-compose.yml
@@ -52,17 +74,21 @@ else
     docker compose -f /root/project/traefik/docker-compose.yml up -d
 
     echo "Копирую файл docker-compose.yml для Portainer..."
-    cp /root/project/portainer_docker-compose.yml /root/project/portainer/docker-compose.yml
-    echo "Заменяю параметры в файле docker-compose.yml для Portainer..."
+    cp /root/project/template/portainer/docker-compose.yml /root/project/portainer/docker-compose.yml
+
+    echo "Заменяю параметры в docker-compose.yml для Portainer..."
     sed -i -e "s|URL|$URL|g" /root/project/portainer/docker-compose.yml
+
     echo "Запускаю контейнеры Portainer..."
     docker compose -f /root/project/portainer/docker-compose.yml up -d
 
     echo "Копирую файл docker-compose.yml для 3x-ui..."
-    cp /root/project/3x-ui_docker-compose.yml /root/project/3x-ui/docker-compose.yml
-    echo "Заменяю параметры в файле docker-compose.yml для 3x-ui..."
+    cp /root/project/template/3x-ui/docker-compose.yml /root/project/3x-ui/docker-compose.yml
+
+    echo "Заменяю параметры в docker-compose.yml для 3x-ui..."
     sed -i -e "s|URL|$URL|g" /root/project/3x-ui/docker-compose.yml
     sed -i -e "s|HOST_SNI|$HOST_SNI|g" /root/project/3x-ui/docker-compose.yml
+
     echo "Запускаю контейнеры 3x-ui..."
     docker compose -f /root/project/3x-ui/docker-compose.yml up -d
 fi
